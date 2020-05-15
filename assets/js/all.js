6c21f844e973ecf7e7010486f4f03f4ac1932846
Jay <jayxxx647@gmail.com>
2020-05-15 18:59:52 +0800

    20200515

---
+++ assets/js/all.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//   $(".accordion-title").click(function(e){
+//       var accordionitem = $(this).attr("data-tab");
+//       $("#"+accordionitem).slideToggle().parent().siblings().find(".accordion-content").slideUp();
+//       $(this).toggleClass("active-title");
+//       $("#"+accordionitem).parent().siblings().find(".accordion-title").removeClass("active-title");
+//       $(".fa-chevron-down",this).toggleClass("chevron-top");
+//       $("#"+accordionitem).parent().siblings().find(".accordion-title .fa-chevron-down").removeClass("chevron-top");
+//   });
+// });
+"use strict";
+//# sourceMappingURL=all.js.map
